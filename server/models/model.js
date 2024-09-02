a89384f5dc308b5079020278f178472c49ecdcec
Anike6381 <anipawar000@gmail.com>
2024-09-02 22:41:32 +0530

    Shubhra added code

--- server/models/model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const TodoSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  todo: { type: String, required: true },
-});
-
-module.exports = mongoose.model("todos", TodoSchema);
